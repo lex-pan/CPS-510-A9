53a231b6aed3d1b1b9fd556120de0a6101d5c449
rogan-d <roganduenas@gmail.com>
2025-11-23 23:44:12 -0500

    Update insert.sql

--- sql-files/insert.sql
+++ sql-files/insert.sql
@@ -42,3 +42,37 @@ INSERT INTO warehouseSchedule VALUES ('Toronto Warehouse Station', 4, 5, 0, '202
 INSERT INTO warehouseSchedule VALUES ('Toronto Warehouse Station', 3, 2, 0, '2020-09-22 14:30', '2020-09-22 18:30');
 INSERT INTO warehouseSchedule VALUES ('Markham Warehouse Station', 4, 5, 0, '2017-09-22 14:30', '2020-09-22 18:30');
 INSERT INTO warehouseSchedule VALUES ('Missisauga Warehouse Station', 3, 2, 0, '2020-09-22 10:30', '2020-09-22 18:30');
+
+-- Customers
+INSERT INTO CUSTOMER
+VALUES ('rogan@gmail.com', 'Rogan', 'Duenas', '131 Phantom Avenue', 'thief123', '1');
+INSERT INTO CUSTOMER
+VALUES ('kevin@gmail.com', 'Kevin', 'Munich', '1101 Yonge Street', 'imamunch15', '2');
+INSERT INTO CUSTOMER
+VALUES ('joker@gmail.com', 'Joe', 'Kerr', '16 Shibuya Road', 't4ke0v3r', '3');
+
+-- Sellers
+INSERT INTO SELLER
+VALUES('paymepls@yahoo.ca', 'BeggingCo', '911 Jays Boulevard', 'PLEASEPLEASEPLEASE', '1');
+INSERT INTO SELLER
+VALUES('Joejoeson@hotmail.com', 'JoesAndStuff', '10 Joseph Street', 'MYNAMEISNOTJOE', '2');
+INSERT INTO SELLER
+VALUES('bryce@papen.com', 'RiceBowels', '113 Brooke Road', 'IMINEVERYTHING', '3');
+
+-- Products
+INSERT INTO PRODUCT
+VALUES('1', 'Calc', '15.99', 'Slang, short for calculator.', '50', '2', 'Joejoeson@hotmail.com');
+INSERT INTO PRODUCT
+VALUES('2', 'Purse', '25.00', 'Please own one of our purses!', '5', '3', 'bryce@papen.com');
+INSERT INTO PRODUCT
+VALUES('3', 'Cards', '8.99', 'These cards are overpriced.', '100', '1', 'paymepls@yahoo.ca');
+
+-- Info for different orders
+INSERT INTO ORDERINF
+VALUES('1', '2', '1', '3', 'gift_card','2025-01-01');
+INSERT INTO ORDERINF
+VALUES('2', '3', '1', '1', 'debit','2025-02-08');
+INSERT INTO ORDERINF
+VALUES('3', '1', '1', '1', 'credit','2025-03-05');
+INSERT INTO ORDERINF
+VALUES('4', '2', '1', '1', 'gift_card', '2025-10-06');
